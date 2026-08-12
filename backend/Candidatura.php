802de39ad0f646d06a800f6f3514266804aa8a27
Phill <filipeabreujlle@gmail.com>
2026-08-12 19:12:35 -0300

    feat: add status validation to Candidatura

--- backend/Candidatura.php
+++ backend/Candidatura.php
@@ -1,12 +1,26 @@
 <?php
 
-class Candidatura {
-   
-public function __construct(
-    private string $empresa,
-    private string $cargo,
-    private string $dataCandidatura,
-    private string $status
-) {
-}
+class Candidatura
+{
+
+    public function __construct(
+        private string $empresa,
+        private string $cargo,
+        private string $dataCandidatura,
+        private string $status
+    ) {}
+
+    public function alterarStatus(string $status): void
+    {
+
+        if ($status !== 'Aprovado' && $status !== 'Reprovado') {
+            throw new InvalidArgumentException('Status inválido.');
+        }
+        $this->status = $status;
+    }
+
+    public function getStatus(): string
+    {
+        return $this->status;
+    }
 }
